bc4e72eba4560a6a5a1571f1c931d29beca59444
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-27 21:30:49 +0300

    feat:add preauthorized annotations.

--- src/main/java/com/springsecurityjwt/controller/SecurityJwtTestController.java
+++ src/main/java/com/springsecurityjwt/controller/SecurityJwtTestController.java
@@ -1,5 +1,6 @@
 package com.springsecurityjwt.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/security/test")
 public class SecurityJwtTestController {
 
+    @PreAuthorize("hasAnyRole('USER', 'MODERATOR', 'SUPER_ADMIN')")
     @GetMapping("/user")
     public String testUser() {
         return "User";
     }
 
+    @PreAuthorize("hasAnyRole('MODERATOR', 'SUPER_ADMIN')")
     @GetMapping("/moderator")
     public String testModerator() {
         return "Moderator";
     }
 
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @GetMapping("/super_admin")
     public String testSuperAdmin() {
         return "SuperAdmin";
