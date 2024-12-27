93713f5874fcca59f5210bf0a858ebde4c46c127
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-27 15:07:25 +0300

    feat:create auth controller.

---
+++ src/main/java/com/springsecurityjwt/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.springsecurityjwt.controller;
+
+import com.springsecurityjwt.dto.LoginDto;
+import com.springsecurityjwt.dto.RegisterDto;
+import com.springsecurityjwt.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginDto loginDto) {
+        return ResponseEntity.ok(authService.login(loginDto));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterDto registerDto) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshAccessToken(@RequestBody String refreshToken) {
+        return ResponseEntity.ok(authService.refreshAccessToken(refreshToken));
+    }
+}
