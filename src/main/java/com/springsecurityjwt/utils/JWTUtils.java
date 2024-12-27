54819164a63baa33ba741211acc140742bb9d673
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-27 13:55:23 +0300

    feat:create jwt utils.

---
+++ src/main/java/com/springsecurityjwt/utils/JWTUtils.java
@@ -0,0 +1,70 @@
+package com.springsecurityjwt.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Component
+public class JWTUtils {
+
+    private final SecretKey secretKey;
+
+    private static final long EXPIRATION_TIME_ACCESS_TOKEN = 24 * 60 * 60 * 1000;
+    private static final long EXPIRATION_TIME_REFRESH_TOKEN = 4 * 24 * 60 * 60 * 1000;
+
+    public JWTUtils() {
+        String secretKey = "mySecretKey";
+        byte[] keyBytes = secretKey.getBytes();
+        this.secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_ACCESS_TOKEN))
+                .claim("roles", userDetails.getAuthorities())
+                .signWith(secretKey)
+                .compact();
+
+    }
+
+    public String generateRefreshToken(HashMap<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_REFRESH_TOKEN))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+        final Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsTFunction.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return userDetails.getUsername().equals(username) && isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+}
